61988ff3fd71a790bcae508ab0e0125e3c04b9da
Claudiu <claudiu725@yahoo.com>
2014-06-02 23:58:34 +0300

    pin

--- pins_big_robot.h
+++ pins_big_robot.h
@@ -32,7 +32,7 @@ const int horizontalArmPin = 15;
 //Vertical arm
 const int verticalArmForwardPin = 22;
 const int verticalArmBackwardPin = 24;
-const int verticalArmEncoderPin = 19;
+const int verticalArmEncoderPin = 18;
 
 //Claw
 const int clawMotorForwardPin = 26;
